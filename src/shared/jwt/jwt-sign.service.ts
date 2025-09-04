f50e36e143b66db962c49f2f296fe8797f2ce5ca
choisangwon <rngus3698@naver.com>
2025-09-04 15:37:55 +0900

    access token 만료시간 15분으로 수정

--- src/shared/jwt/jwt-sign.service.ts
+++ src/shared/jwt/jwt-sign.service.ts
@@ -19,7 +19,7 @@ export class JwtSignService {
   }): Promise<string> {
     return await this.jwtService.signAsync(payload, {
       secret: this.configService.get<Configuration['jwt']>('jwt').access_secret,
-      expiresIn: '1m',
+      expiresIn: '15m',
     });
   }
 
